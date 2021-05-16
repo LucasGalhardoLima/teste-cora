a857a63cdb6c81a71d7228df4d152471f7625a04
Lucas Galhardo Lima <lima.galhardo@gmail.com>
2021-05-16 13:17:09 -0300

    feat(Index, Header): started project and added header component with buttons and search input

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="container mx-auto py-6">
+      <h1 class="text-2xl">Extrato</h1>
+    </div>
+    <hr />
+    <div class="container mx-auto py-10">
+      <Nuxt />
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'Index',
+}
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <Header />
+      <h1 class="title primary">teste-cora</h1>
+    </div>
+  </div>
+</template>
