190160272a35d469a64102a1717a536684542b91
Lucas Galhardo Lima <lima.galhardo@gmail.com>
2021-05-17 22:25:47 -0300

    feat(History): created static history card

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <script>
 export default {
   name: 'Index',
+  data: () => ({
+    items: [1, 2, 3, 4],
+  }),
 }
 </script>
 
 <template>
   <div class="container">
     <div>
-      <Header />
-      <h1 class="title primary">teste-cora</h1>
+      <Header class="pb-10" />
+      <History v-for="(item, pos) in items" :key="item" :item-pos="pos" />
     </div>
   </div>
 </template>
